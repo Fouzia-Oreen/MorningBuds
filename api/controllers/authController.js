b803412f423e7be0ba52fc4beb561d2b848abf0c
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-09-23 14:08:55 +0600

    error middleware added

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,16 +1,19 @@
 import User from "../models/userModels.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body
     if (!username || 
         !email || 
         !password || 
         username === "" ||
         password === "" || 
-        email === "") {
-        return res.status(400).json({message: "All fields required"})
+        email === "") 
+    {
+        next(errorHandler(400, 'All fields are required'));
     }
+
     const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({
         username, 
@@ -21,7 +24,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json("Signup successfull")
     } catch (error) {
-        res.status(500).json({message:error.message})
+        next(error)
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import express from "express";
 import mongoose from 'mongoose';
-import userRoute from './routes/userRoute.js'
-import authRoute from './routes/authRoute.js'
+import authRoute from './routes/authRoute.js';
+import userRoute from './routes/userRoute.js';
 const app = express()
 dotenv.config();
 
@@ -11,8 +11,17 @@ mongoose.connect(process.env.MONGO_URI)
   console.log('Mongo db is connected');
 })
 
-
+// middleware
 app.use(express.json())
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 
 
