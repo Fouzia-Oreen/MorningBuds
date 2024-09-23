a2f5206b765499b5c80668f8f49c2310d5d8299e
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-09-23 14:09:57 +0600

    client Ui structure codebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,38 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
 
 
   return (
-    <h1>hello morning-buds</h1>
+    <BrowserRouter>
+      {/* <ScrollToTop /> */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        {/* <Route path='/search' element={<Search />} /> */}
+        {/* <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route> */}
+        {/* <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+        </Route> */}
+
+        {/* <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+      
   )
 }
 
