e399183444d6c230f6ae5f6ec68937cced2e2636
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-09-24 21:12:05 +0600

    Footer code added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,82 @@
+ 
+/* eslint-disable react/jsx-key */
+
+import { FaDribbble, FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa"
+import { Link } from "react-router-dom"
+import { Logo } from "./Button"
+
+const footerData = [
+  {
+    title: "About",
+    link1: "Community",
+    link2: "Announcement",
+  },
+  {
+    title: "Accounts",
+    link1:"Student-Results",
+    link2:"Student-Fee",
+  },
+  {
+    title: "Team",
+    link1:"Teachers",
+    link2:"Staff",
+  },
+  {
+    title: "Contact",
+    link1:"+099 9945 321",
+    link2:"morningbuds@gmail.com",
+  }
+]
+const footerIcon = [
+  {
+    href : "/",
+    icon : <FaFacebook />
+  },
+  {
+    href : "/",
+    icon : <FaInstagram />
+  },
+  {
+    href : "/",
+    icon : <FaTwitter />
+  },
+  {
+    href :'https://github.com/Fouzia-Oreen',
+    icon : <FaGithub />
+  },
+  {
+    href : "/",
+    icon : <FaDribbble />
+  },
+]
+
+const Footer = () => {
+  return (
+    <div className="border-t-2 border-neutral-400 py-2">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-2">
+          <Logo />
+          <div>
+            <div className='grid w-full justify-evenly sm:flex md:grid-cols-2 lg:grid-cols-3 '>
+                {footerData.map((data) => 
+                  <div className='mt-5 flex-flex-col gap-5 text-start p-4'>
+                  <h2 className="text-lg font-bold text-neutral-700">{data.title}</h2>
+                  <Link to="/"><p className="text-sm mt-2">{data.link1}</p></Link>
+                  <Link to="/"><p className="text-sm mt-2">{data.link2}</p></Link>           
+                  </div>
+                )}
+            </div>
+          </div>   
+      </div>
+
+      <div className='w-full flex lg:flex-row flex-col items-center gap-4 justify-center border-t-[1px] border-neutral-400 mt-6 pt-6 font-semibold text-neutral-600 md:flex-row md:justify-evenly flex-shrink border-opacity-70'>
+          <Link to='#'>Codoreen.io <span>{new Date().getFullYear()}</span></Link>
+          <div className="flex gap-6 sm:justify-center text-neutral-500  text-lg">
+          {footerIcon.map((icon) => <a href={icon.href} className="hover:text-neutral-600 transition duration-200">{icon.icon}</a>
+           )}
+          </div>
+        </div>
+    </div>
+    
+  );
+}
+export default Footer
