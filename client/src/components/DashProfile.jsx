5067e5b641a7f78a49025bea9d8a4bcc8560a3b5
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-06 22:24:34 +0600

    add delete user UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@
 
 import { getStorage } from 'firebase/storage';
 import { app } from '../firebase';
-import { Alert, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput , ModalBody, ModalHeader, Modal} from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -19,6 +19,7 @@ import {
 } from '../app/user/userSlice';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 //import {CommonButton} from "./Button"
 
 const DashProfile = () => {
@@ -119,6 +120,23 @@ const DashProfile = () => {
           setUpdateUserError(error.message);
         }
       };
+      const handleDeleteUser = async () => {
+        setShowModal(false);
+        try {
+          dispatch(deleteUserStart());
+          const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+            method: 'DELETE',
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            dispatch(deleteUserFailure(data.message));
+          } else {
+            dispatch(deleteUserSuccess(data));
+          }
+        } catch (error) {
+          dispatch(deleteUserFailure(error.message));
+        }
+      };
 
     return (
       <section className='h-screen mx-auto  flex  items-center justify-center  bg-slate-300 dark:bg-dark-200 '>
@@ -162,9 +180,48 @@ const DashProfile = () => {
           </div>
         </form>
         <div className="flex items-center justify-between gap-12 font-semibold">
-          <span className="cursor-pointer text-red-700">Delete Account</span>
+          <span className="cursor-pointer text-red-700" onClick={() => setShowModal(true)}>Delete Account</span>
           <span className="cursor-pointer">Sign Out</span>
         </div>
+        {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+          {updateUserError}
+        </Alert>
+      )}
+      {error && (
+        <Alert color='failure' className='mt-5'>
+          {error}
+        </Alert>
+      )}
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete your account?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
+                Yes, I&apos;m sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
       </div> 
       </section>
     )
